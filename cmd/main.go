c6b8d7ebb8ca6b9c058a68e401607c18f13d8d0b
Nicolás Rodríguez <nicrodriguezval@gmail.com>
2024-03-17 22:25:18 -0500

    Code formatted

--- cmd/main.go
+++ cmd/main.go
@@ -10,41 +10,41 @@ import (
 )
 
 type Template struct {
-  templates *template.Template
+	templates *template.Template
 }
 
 func (t *Template) Render(w io.Writer, name string, data any, c echo.Context) error {
-  return t.templates.ExecuteTemplate(w, name, data)
+	return t.templates.ExecuteTemplate(w, name, data)
 }
 
 func NewTemplate() *Template {
-  return &Template{
-    templates: template.Must(template.ParseGlob("views/*.html")),
-  }
+	return &Template{
+		templates: template.Must(template.ParseGlob("views/*.html")),
+	}
 }
 
 type Count struct {
-  Count int
+	Count int
 }
 
 func main() {
 	e := echo.New()
-  e.Renderer = NewTemplate()
+	e.Renderer = NewTemplate()
 
-  // middlewares
-  e.Use(middleware.Logger())
+	// middlewares
+	e.Use(middleware.Logger())
 
-  // Routes
-  var count Count
+	// Routes
+	var count Count
 
-  e.GET("/", func(c echo.Context) error {
-    return c.Render(http.StatusOK, "index", count)
-  })
+	e.GET("/", func(c echo.Context) error {
+		return c.Render(http.StatusOK, "index", count)
+	})
 
-  e.POST("/count", func(c echo.Context) error {
-    count.Count++
-    return c.Render(http.StatusOK, "count", count)
-  })
+	e.POST("/count", func(c echo.Context) error {
+		count.Count++
+		return c.Render(http.StatusOK, "count", count)
+	})
 
-  e.Logger.Fatal(e.Start(":1323"))
+	e.Logger.Fatal(e.Start(":1323"))
 }
